36fe5a82081b6f20e9c9454a99651f82978dca69
discoder <discoder.hello@gmail.com>
2018-03-27 17:08:59 +0300

    ROUTER: Added routes. Added 404 page.

---
+++ src/components/page/page-not-found.vue
@@ -0,0 +1,5 @@
+<template lang="html">
+    <div class="components-list">
+        <h1>Page Not Found</h1>
+    </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,34 @@
-import Vue              from 'vue'
-import Router           from 'vue-router'
+import Vue          from 'vue'
+import Router       from 'vue-router'
+import PageNotFound from '../components/page/page-not-found.vue'
+import PageHome     from '../components/page/page-home.vue'
+import PageMusic    from '../components/page/page-music.vue'
+import PageParties  from '../components/page/page-parties.vue'
 
 Vue.use(Router)
 
-export default new Router()
+export default new Router({
+    routes: [
+        {
+            path: '*',
+            name: 'PageNotFound',
+            component: PageNotFound
+        },
+        {
+            path: '/',
+            name: 'PageHome',
+            component: PageHome
+        },
+        {
+            path: '/music',
+            name: 'PageHome',
+            component: PageMusic
+        },
+        {
+            path: '/parties',
+            name: 'PageHome',
+            component: PageParties
+        },
+    ],
+    mode: 'history'
+})
